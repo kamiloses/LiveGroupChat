075174a4cae003031c94f1933dc1c210d012cdb7
kamil <kamil.kurzaj04@gmail.com>
2025-04-28 08:07:18 +0200

    Addes Models

---
+++ LiveGroupChat/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace LiveGroupChat.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string? Text { get; set; }
+    public DateTime Created { get; set; }
+    public int? UserId { get; set; }
+    
+    
+    
+    
+}
