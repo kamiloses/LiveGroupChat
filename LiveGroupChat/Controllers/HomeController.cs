aa141d2feded9375000a06ab9d2ef5cc2c5be87a
kamil <kamil.kurzaj04@gmail.com>
2025-04-30 09:41:53 +0200

    changed home css

--- LiveGroupChat/Controllers/HomeController.cs
+++ LiveGroupChat/Controllers/HomeController.cs
@@ -11,14 +11,26 @@ public class HomeController : Controller{
     
     [Route("/home")]
     public ActionResult Home() {
+        if (messages.Count > 5)messages.Clear();
+        
+
+        {
+            
+        }
         
         return View(messages);
     }
     [HttpPost]
     [Route("/home")]
-    public ActionResult WriteMessage(MessageViewModel message)
-    {
-        message.User = new UserViewModel(){FirstName = "Jan", LastName = "Kowalski"};
+        public ActionResult WriteMessage(MessageViewModel message)
+        {
+            
+            
+            Random random1= new Random();
+            if (random1.Next(2) == 0)    message.User = new UserViewModel(){FirstName = "Jan", LastName = "Kowalski"};
+            else message.User = new UserViewModel(){FirstName = "Maciej", LastName = "Nowak"};
+            
+            
        Random random= new Random();
         if (random.Next(2) == 0) message.Created = DateTime.Now.AddDays(-1);
         else message.Created = DateTime.Now;
