19414f50d89a9b9e73bd338e16f9a52bc3846239
kamil <kamil.kurzaj04@gmail.com>
2025-04-30 10:24:20 +0200

    initial implementation of ws

---
+++ LiveGroupChat/wwwroot/js/websocket.js
@@ -0,0 +1,21 @@
+﻿let stompClient = null
+let chatId = null;
+let currentSubscription = null;
+let isPublicChat="false";
+
+function connectToWebSocket() {
+
+
+    const socket = new SockJS('http://localhost:8080/ws');
+    stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+
+        console.log('Connected: ' + frame);
+        
+       stompClient.subscribe('/topic/public/group', function (message) {
+           var activeUsers = JSON.parse(message.body);
+
+
+                
+            })}
