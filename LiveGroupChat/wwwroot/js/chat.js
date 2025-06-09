8f928608cddde4219ec8d57c00b107c612e13ef3
kamil <kamil.kurzaj04@gmail.com>
2025-06-09 08:00:26 +0200

    Added opportunity to send message in realtime (currently as logs)

--- LiveGroupChat/wwwroot/js/chat.js
+++ LiveGroupChat/wwwroot/js/chat.js
@@ -4,12 +4,36 @@ window.onload = function () {
     connectToWebsockets();}
 }
 
+
+
+let connection;
+
 function connectToWebsockets() {
     console.log("Trying to connect to SignalR...");
     connection = new signalR.HubConnectionBuilder()
         .withUrl("/ws")
         .build();
 
+    connection.on("ReceiveMessage", (username, message, messageId) => {
+        console.log(message);
+    });
 
+    connection.start()
+        .then(() => {
+            console.log("Connected to SignalR server");
+
+            document.querySelector(".send-btn").addEventListener("click", function (event) {
+                event.preventDefault();
+                let message = document.querySelector(".chat-input").value;
+                connection.invoke("SendMessage", message)
+                    .then(() => {
+                        console.log("SENT MESSAGE");
+                    })
+                    .catch(err => {
+                        console.error("Error sending message: ", err);
+                    });
+            });
+
+        })
+        .catch(err => console.error("SignalR connection error: ", err));
 }
-
